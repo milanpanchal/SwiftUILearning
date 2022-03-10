da7e1029e07727418bb11a9806d844120fb1a500
Milan Panchal <milan@antarcticaglobal.com>
2022-03-10 14:36:01 +0530

    Initial Commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Milankumar Panchal on 10/03/22.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
