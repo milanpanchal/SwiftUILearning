e3d6bf5f5f3277416adf350733ff2c22c3a83ea9
Milan Panchal <milan@antarcticaglobal.com>
2022-03-10 14:50:30 +0530

    feat: CWC Lesson 5: War Card Game UI Construction

--- WarCardGame/ContentView.swift
+++ WarCardGame/ContentView.swift
@@ -9,8 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background").ignoresSafeArea()
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                    Image("card4")
+                    Spacer()
+                }
+                Spacer()
+                Image("dealbutton")
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding(.bottom, 10)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding(.bottom, 10)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)                        
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
     }
 }
 
