35982692bf7812fe6d4b15133932c37904152f3f
Milan Panchal <sam07it22@gmail.com>
2022-03-18 13:16:49 +0530

    feat: SwiftUI GridLayout Empty Project

---
+++ SwiftUIGridLayout/SwiftUIGridLayout/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIGridLayout
+//
+//  Created by Milankumar Panchal on 18/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIGridLayout/SwiftUIGridLayout/SwiftUIGridLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGridLayoutApp.swift
+//  SwiftUIGridLayout
+//
+//  Created by Milankumar Panchal on 18/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGridLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
