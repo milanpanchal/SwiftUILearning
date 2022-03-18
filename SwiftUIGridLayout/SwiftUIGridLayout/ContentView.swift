0360530ec2744b24c45146effa8db2ff3dfab2da
Milan Panchal <sam07it22@gmail.com>
2022-03-18 13:42:31 +0530

    feat: LazyVGrid with Scroll view

--- SwiftUIGridLayout/SwiftUIGridLayout/ContentView.swift
+++ SwiftUIGridLayout/SwiftUIGridLayout/ContentView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var columnGrid: [GridItem] = [GridItem(.adaptive(minimum: 50))]
+//  private var columnGrid: [GridItem] = Array(repeating: GridItem(.flexible(), spacing: 5), count: 5)
+    private var symbols = ["keyboard", "hifispeaker.fill", "printer.fill", "tv.fill", "desktopcomputer", "headphones", "tv.music.note", "mic", "plus.bubble", "video"]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ScrollView {
+            LazyVGrid(columns: columnGrid) {
+                ForEach((0...99), id: \.self) { idx in
+                    Image(systemName: symbols[idx % symbols.count])
+                        .font(.system(size: 30))
+                        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 50)
+                        .background(Color.random)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+            }.padding()
+        }
+        .background(LinearGradient(colors: [Color("GradientYellowStart"), Color("GradientYellowEnd")],
+                                   startPoint: .topLeading,
+                                   endPoint: .bottomTrailing))
+        
+    }
+}
+
+// MARK: - Generatin random color in SwiftUI
+extension Color {
+    static var random: Color {
+        return Color(
+            red: .random(in: 0...1),
+            green: .random(in: 0...1),
+            blue: .random(in: 0...1)
+        )
     }
 }
 
